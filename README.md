8f1e165d5f3fb87a357e566a84ed6da14bf19f84
Tim Wheeler <tim@timwheeler.com>
2018-04-29 19:33:57 +0000

    Edited car-rental/

---
+++ README.md
@@ -0,0 +1,8 @@
+# Car Rental Application
+
+## Feature Backlog
+
+- Create popup modal instead of default browser alerts
+- Add date selector
+- Add (fake) payment integration
+- Link info to a database
---
+++ js/custom.js
@@ -0,0 +1,31 @@
+/* global $ */
+
+//Date picker script
+
+$(function(){
+  // implementation of custom elements instead of inputs
+	var startDate = new Date(2012, 1, 20);
+	var endDate = new Date(2012, 1, 25);
+	$('#dp4').fdatepicker()
+		.on('changeDate', function (ev) {
+		if (ev.date.valueOf() > endDate.valueOf()) {
+			$('#alert').show().find('strong').text('The start date can not be greater then the end date');
+		} else {
+			$('#alert').hide();
+			startDate = new Date(ev.date);
+			$('#startDate').text($('#dp4').data('date'));
+		}
+		$('#dp4').fdatepicker('hide');
+	});
+	$('#dp5').fdatepicker()
+		.on('changeDate', function (ev) {
+		if (ev.date.valueOf() < startDate.valueOf()) {
+			$('#alert').show().find('strong').text('The end date can not be less then the start date');
+		} else {
+			$('#alert').hide();
+			endDate = new Date(ev.date);
+			$('#endDate').text($('#dp5').data('date'));
+		}
+		$('#dp5').fdatepicker('hide');
+	});
+});
